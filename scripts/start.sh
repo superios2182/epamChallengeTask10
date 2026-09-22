5c58e0d7663585661ba322315d891c53961bed0d
superios2182 <76697533+superios2182@users.noreply.github.com>
2026-09-22 18:27:21 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user/flask-app
-# Запускаем Flask в фоновом режиме на 8000 порту в соответствии с Target Group
-nohup python3 -m flask run --host=0.0.0.0 --port=8000 > /dev/null 2>&1 &
+
+# Останавливаем старые процессы python
+pkill -f "python" || true
+
+# Указываем Flask файл приложения и запускаем на порту 8000
+export FLASK_APP=app.py
+nohup python3 -m flask run --host=0.0.0.0 --port=8000 > /home/ec2-user/app.log 2>&1 &
